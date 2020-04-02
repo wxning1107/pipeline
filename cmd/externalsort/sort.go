f3987babfab498f256dce2bf104e8418cc9138a4
wxning <wxning@canway.net>
2020-04-02 21:40:00 +0800

    update internal sort

--- cmd/externalsort/sort.go
+++ cmd/externalsort/sort.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"os"
 	"pipeline/pipeline"
+	"strconv"
 )
 
 func main() {
-	p := createPipeline("small.in", 512, 4)
-	writeToFile(p, "small.out")
-	printFile("small.out")
+	p := createNetworkPipeline("large.in", 800000000, 4)
+	writeToFile(p, "large.out")
+	printFile("large.out")
 }
 
 func printFile(filename string) {
@@ -21,8 +22,13 @@ func printFile(filename string) {
 	defer file.Close()
 
 	p := pipeline.ReaderSource(file, -1)
+	count := 0
 	for v := range p {
 		fmt.Println(v)
+		count++
+		if count >= 100 {
+			break
+		}
 	}
 }
 
@@ -41,6 +47,7 @@ func writeToFile(p <-chan int, filename string) {
 
 func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
 	chunkSize := fileSize / chunkCount
+	pipeline.Init()
 
 	sortResults := []<-chan int{}
 	for i := 0; i < chunkCount; i++ {
@@ -55,5 +62,32 @@ func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
 		sortResults= append(sortResults, pipeline.InMenSort(source))
 	}
 
+	return pipeline.MergeN(sortResults...)
+}
+
+func createNetworkPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	pipeline.Init()
+
+	sortAddr := []string{}
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		// file要从这一块开始读，所以file要移动一下
+		file.Seek(int64(chunkSize * i), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+
+		addr := ":" + strconv.Itoa(7000 + i)
+		pipeline.NetWorkSink(addr, pipeline.InMenSort(source))
+		sortAddr = append(sortAddr, addr)
+	}
+
+	sortResults := []<-chan int{}
+	for _, addr := range sortAddr {
+		sortResults = append(sortResults, pipeline.NetworkSource(addr))
+	}
 	return pipeline.MergeN(sortResults...)
 }
--- pipeline/nodes.go
+++ pipeline/nodes.go
@@ -2,11 +2,18 @@ package pipeline
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 	"math/rand"
 	"sort"
+	"time"
 )
 
+var startTime time.Time
+
+func Init() {
+	startTime = time.Now()
+}
 func ArraySource(a ...int) <-chan int {
 	out := make(chan int)
 	go func() {
@@ -19,15 +26,17 @@ func ArraySource(a ...int) <-chan int {
 }
 
 func InMenSort(in <-chan int) <-chan int {
-	out := make(chan int)
+	out := make(chan int, 1024)
 	go func() {
 		// Read into memory
 		a := []int{}
 		for v := range in {
 			a = append(a, v)
 		}
+		fmt.Println("Read done", time.Now().Sub(startTime))
 		// Sort
 		sort.Ints(a)
+		fmt.Println("InMemSort done", time.Now().Sub(startTime))
 		//Output
 		for _, v := range a {
 			out <- v
@@ -39,7 +48,7 @@ func InMenSort(in <-chan int) <-chan int {
 }
 
 func Merge(in1, in2 <-chan int) <-chan int {
-	out := make(chan int)
+	out := make(chan int, 1024)
 	go func() {
 		v1, ok1 := <-in1
 		v2, ok2 := <-in2
@@ -53,12 +62,13 @@ func Merge(in1, in2 <-chan int) <-chan int {
 			}
 		}
 		close(out)
+		fmt.Println("Merge done", time.Now().Sub(startTime))
 	}()
 	return out
 }
 
 func ReaderSource(reader io.Reader, chunkSize int) <-chan int {
-	out := make(chan int)
+	out := make(chan int, 1024)
 	go func() {
 		buffer := make([]byte, 8)
 		bytesRead := 0
